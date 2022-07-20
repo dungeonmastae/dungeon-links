7752447e9944b2c3b6579d25056d8db7d4d27148
dungeonmastae <58392802+dungeonmastae@users.noreply.github.com>
2022-07-20 23:28:10 +0530

    Update st_functions.py

--- st_functions.py
+++ st_functions.py
@@ -53,6 +53,17 @@ def st_button(icon, url, label, iconsize):
                 {label}
             </a>
         </p>'''
+        
+       
+        
+    elif icon == 'codeforces':
+        button_code = f'''
+        <p>
+            <a href={url} class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">
+                 <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M4.5 7.5A1.5 1.5 0 0 1 6 9v10.5A1.5 1.5 0 0 1 4.5 21h-3C.673 21 0 20.328 0 19.5V9c0-.828.673-1.5 1.5-1.5h3zm9-4.5A1.5 1.5 0 0 1 15 4.5v15a1.5 1.5 0 0 1-1.5 1.5h-3c-.827 0-1.5-.672-1.5-1.5v-15c0-.828.673-1.5 1.5-1.5h3zm9 7.5A1.5 1.5 0 0 1 24 12v7.5a1.5 1.5 0 0 1-1.5 1.5h-3a1.5 1.5 0 0 1-1.5-1.5V12a1.5 1.5 0 0 1 1.5-1.5h3z"/></svg>
+                {label}
+        </p>''' 
+    
     elif icon == 'leetcode' or icon == 'codechef' or icon == 'codeforces':
         button_code = f'''
         <p>
