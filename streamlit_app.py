8ee4d7efac62ee3ec71c75b3fcf87883afeaccb2
dungeonmastae <58392802+dungeonmastae@users.noreply.github.com>
2022-07-20 21:50:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from PIL import Image
 load_css()
 
 col1, col2, col3 = st.columns(3)
-col2.image(Image.open('dp.png'))
+col2.image(Image.open('sJyjnYUx_400x400.jpg'))
 
 st.header('Arpit Dutt Dixit, CS.')
 
